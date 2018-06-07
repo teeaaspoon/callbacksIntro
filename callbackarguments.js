c83fd47325a10477ca54ed95ecb455ae2af700b9
teeaaspoon <tommypoon36@gmail.com>
2018-06-07 16:32:27 +0000

    refactored to use forEach Loop

--- callbackarguments.js
+++ callbackarguments.js
@@ -1,10 +1,18 @@
 // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//     for (var i = 0; i < arr.length; i++) {
+//         if (arr[i] === "Waldo") {
+//             found(i); // execute callback
+//         }
+//     }
+// }
+
 function findWaldo(arr, found) {
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i] === "Waldo") {
-            found(i); // execute callback
+    arr.forEach(function(element, index) {
+        if (element === "Waldo") {
+            found(index);
         }
-    }
+    });
 }
 
 function actionWhenFound(index) {
@@ -14,3 +22,5 @@ function actionWhenFound(index) {
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
 
 //Modify the callback function in the previous example so that logs the index of the array where Waldo is found, ie. "Found Waldo at index 2!". (You will need to modify actionWhenFound to receive the index.)
+
+//Refactor the function findWaldo to use the forEach() method instead of a for loop.
